b965b5e1a8b71cc9037ab13471b6fbec29b43156
mteguhpro <80159944+mteguhpro@users.noreply.github.com>
2023-07-30 20:29:40 +0700

    awalan

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      "babel-preset-expo",
+      "module:metro-react-native-babel-preset"
+    ],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+      require.resolve("expo-router/babel"),
+      "react-native-paper/babel",
+    ],
+  };
+};
